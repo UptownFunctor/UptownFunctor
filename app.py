9f720b8cac5010d0c1b5137e8ee5d18761a94eb6
Jimmy Du <jimmydu4444@gmail.com>
2020-11-30 19:31:27 -0800

    delimiter mvp, semicolon only

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, session, redirect
+from flask import Flask, render_template, request, make_response, jsonify, session, redirect
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -10,9 +10,19 @@ bootstrap = Bootstrap(app)
 def hello_world():
     return render_template("index.html")
 
-@app.route('/delimiter_remover')
+@app.route('/delimiter_remover', methods=['GET','POST'])
 def delimiter_remover():
-    return render_template("delimiter_remover.html")
+
+    if request.method == 'POST':
+        print("processing text")
+
+        text = request.get_json()['value']
+        print(text.strip())
+
+        res = ' '.join(text.split(';')).strip()
+        return make_response(jsonify({'message': res}), 200)
+    else:
+        return render_template("delimiter_remover.html")
 
 @app.route('/hotdog')
 def hotdog():
--- static/scripts/delimiter_remover.js
+++ static/scripts/delimiter_remover.js
@@ -1,3 +1,20 @@
-document.getElementById('input_btn').addEventListener('click',function(e){
+async function delimit_text() {
+    let text = document.getElementById('input_form').value
+
+    let response = await fetch('/delimiter_remover', {
+        method: "POST",
+        body: JSON.stringify({value:text}),
+        headers: {
+            'content-type':'application/json'
+        }
+    })
+
+    let res = await response.json()
+
+    document.getElementById('output_form').value = res['message']
     document.getElementById('output_form_container').style.visibility="visible"
-})
+
+}
+
+document.getElementById('input_btn').addEventListener('click',delimit_text)
+
